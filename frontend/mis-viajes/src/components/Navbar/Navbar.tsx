8ba942f9890a1aaa24c94db704375da3e5681319
iCharlieAraiza <eliptikman1234@gmail.com>
2023-07-09 20:51:37 -0600

    Update nav container styles

--- frontend/mis-viajes/src/components/Navbar/Navbar.tsx
+++ frontend/mis-viajes/src/components/Navbar/Navbar.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import Logo from "../../assets/img/logo.svg";
+import { HiMenu } from "react-icons/hi";
 
 const Navbar = () => {
   return (
-    <nav>Navbar</nav>
-  )
-}
+    <nav className="navbar">
+      <div className="fluid-container">
+        <div className="navbar__menu">
+          <div className="navbar__btn">
+            <div className="btn">
+                <HiMenu />
+            </div>
+          </div>
+          <div className="navbar__logo">
+            <img src={Logo} alt="logo" />
+          </div>
+          <div className="navbar__user">
+            <div className="navbar_user--avatar">
+                <img
+                    src="https://avatars.githubusercontent.com/u/11501555?v=4"
+                    alt="avatar"    
+                />
+            </div>
+            <div className="navbar__user--name">carlozaraiza</div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
