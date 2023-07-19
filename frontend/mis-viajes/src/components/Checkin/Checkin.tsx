4b578dd56ca16cfb32e7671534faf636f360cfd2
iCharlieAraiza <eliptikman1234@gmail.com>
2023-07-18 23:47:52 -0600

    Update styles

--- frontend/mis-viajes/src/components/Checkin/Checkin.tsx
+++ frontend/mis-viajes/src/components/Checkin/Checkin.tsx
@@ -2,7 +2,23 @@ const Checkin = () => {
   return (
     <section className="checkin-container">
       <h1>Buscar lugar</h1>
-      <form action=""></form>
+      <p className="checkin-container__subtitle">
+        Busca el lugar donde te encuentras
+      </p>
+      <div className="checkin-container__input-container">
+        <input type="text" placeholder="Buscar lugar" />
+      </div>
+      <div className="checkin-container__list">
+        <div className="checkin-container__list-item">
+          <div className="checkin-container__list-item__icon">
+            <i className="fas fa-map-marker-alt"></i>
+          </div>
+            <div className="checkin-container__list-item__info">
+                <h3>Tour Eiffel</h3>
+                <p>París, Francia</p>
+            </div>
+        </div>
+      </div>
     </section>
   );
 };
